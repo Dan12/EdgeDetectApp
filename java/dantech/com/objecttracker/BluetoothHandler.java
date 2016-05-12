1b96fd42ff48dbfc37030f36ef6088f720c6413f
Dan12 <dantech88@yahoo.com>
2016-05-11 22:49:07 -0500

    added better bluetooth open method

--- java/dantech/com/objecttracker/BluetoothHandler.java
+++ java/dantech/com/objecttracker/BluetoothHandler.java
@@ -1,8 +1,10 @@
 package dantech.com.objecttracker;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.widget.Toast;
 import java.io.IOException;
@@ -51,40 +53,66 @@ public class BluetoothHandler {
 
         final Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         stopDeviceFinder = false;
+        if (pairedDevices.size() > 0) {
+            CharSequence devices[] = new CharSequence[pairedDevices.size()];
+            int i = 0;
+            for (BluetoothDevice device : pairedDevices) {
+                devices[i] = device.getName();
+                i++;
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(launcher.getApplicationContext());
+            builder.setTitle("Pick a device");
+            builder.setItems(devices, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    int k = 0;
+                    for (BluetoothDevice device : pairedDevices) {
+                        if(i == k) {
+                            tryConnection(device);
+                            break;
+                        }
+                        k++;
+                    }
+
+                }
+            });
+
+            builder.show();
+        }
+    }
+
+    void tryConnection(BluetoothDevice tempDiv){
+        final BluetoothDevice device = tempDiv;
         Thread openDeviceThread = new Thread(new Runnable() {
             public void run() {
-                if (pairedDevices.size() > 0) {
-                    boolean success = false;
-                    int tryNum = 1;
-                    for (BluetoothDevice device : pairedDevices) {
-                        toastMessage("Bluetooth try "+tryNum+"/"+pairedDevices.size());
-                        if(stopDeviceFinder) {
-                            System.out.println("Stopping device finder");
-                            break;
-                        }
-                        System.out.println(device.getName());
-                        if (mmOutputStream != null) {
-                            success = true;
-                            break;
-                        }
-                        mmDevice = device;
-                        try {
-                            openBT();
-                            //System.out.println("success here");
-                            success = true;
-                            break;
-                        } catch (IOException e) {
-                            //System.out.println("Failed");
-                        }
-                        tryNum++;
+                boolean success = false;
+                for (int i = 0; i < 1; i++) {
+                    toastMessage("Bluetooth try "+device.getName());
+                    if(stopDeviceFinder) {
+                        System.out.println("Stopping device finder");
+                        break;
                     }
-                    if (!success)
-                        toastMessage("Failed To Open Any Device");
-                    else{}
-                        //System.out.println("Bluetooth Device Found");
+                    System.out.println(device.getName());
+                    if (mmOutputStream != null) {
+                        success = true;
+                        break;
+                    }
+                    mmDevice = device;
+                    try {
+                        openBT();
+                        //System.out.println("success here");
+                        success = true;
+                        break;
+                    } catch (IOException e) {
+                        //System.out.println("Failed");
+                    }
                 }
+                if (!success)
+                    toastMessage("Failed To Open The Device");
+                else{}
+                //System.out.println("Bluetooth Device Found");
                 findingBluetooth = false;
-                launcher.getDrawView().getMenu().btDone();
             }
         });
         openDeviceThread.start();
